5676103b7eaa149a057843e1351eec9394442641
Amrit Kumar <kumaramrit38@gmail.com>
2018-12-25 16:06:29 +0530

    post api request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular6project';
+  userName: any = '';
+  password: any = '';
+  cpassword: any = '';
+
+  constructor(private http: HttpClient){
+//     this.http.post("https://jsonplaceholder.typicode.com/posts",
+//     {
+//         "title": "Customer004",
+//         "body": "customer",
+//         "userId": 1
+//     })
+//     .subscribe(
+//         data => {
+//             console.log("POST Request is successful ", data);
+//         },
+//         error => {
+//             console.log("Error", error);
+//         }
+//     );  
+//   }
+  
+  this.http.post("http://localhost:8000/users/register",
+  {
+      "username": "name11",
+      "email": "email1@gmal.com",
+      "password": 'password11'
+  })
+  .subscribe(
+      data => {
+          console.log("POST2 Request is successful ", data);
+      },
+      error => {
+          console.log("Error", error);
+      }
+  );  
+}
+
+
 }
